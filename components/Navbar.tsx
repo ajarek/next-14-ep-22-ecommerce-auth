dfd6829df17005228a891308f3feb474583800e4
Ajarek <ajarek@poczta.onet.pl>
2023-12-23 00:47:07 +0100

    added white icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import React, { useState } from 'react'
-import { Button } from './ui/button'
+import { useTheme } from 'next-themes'
 import Link from 'next/link'
 import Image from 'next/image'
+import { ModeToggle } from './mode-toggle'
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -12,16 +13,31 @@ const Navbar = () => {
     { href: '/shop', label: 'Shop' },
     // { href: '/login', label: 'Login' },
   ]
+  const { theme, setTheme } = useTheme()
   return (
     <>
       <header className='  max-w-[1440px] mx-auto z-10 w-full'>
-        <nav className='h-24 flex justify-between items-center max-container  px-8 py-2'>
+        <nav className='h-24 flex justify-between items-center max-container  px-12 py-2'>
           <div>
             <Link
               href='/'
               className='text-3xl font-bold'
             >
-              Logo
+              {theme === 'dark' ? (
+                <Image
+                  src={'/logo-white.svg'}
+                  alt='icon'
+                  width={170}
+                  height={28}
+                />
+              ) : (
+                <Image
+                  src={'/logo-black.svg'}
+                  alt='icon'
+                  width={170}
+                  height={28}
+                />
+              )}
             </Link>
           </div>
 
@@ -42,6 +58,7 @@ const Navbar = () => {
             >
               Login
             </Link>
+            <ModeToggle />
           </ul>
 
           <div
@@ -52,14 +69,22 @@ const Navbar = () => {
           >
             {isMenuOpen ? (
               <Image
-                src={'/assets/icons/close.svg'}
+                src={
+                  theme === 'dark'
+                    ? '/assets/icons/close-white.svg'
+                    : '/assets/icons/close.svg'
+                }
                 alt='icon'
                 width={40}
                 height={40}
               />
             ) : (
               <Image
-                src={'/assets/icons/menu.svg'}
+                src={
+                  theme === 'dark'
+                    ? '/assets/icons/menu-white.svg'
+                    : '/assets/icons/menu.svg'
+                }
                 alt='icon'
                 width={40}
                 height={40}
@@ -70,12 +95,12 @@ const Navbar = () => {
       </header>
       {isMenuOpen && (
         <div className='flex justify-end'>
-          <ul className='absolute w-1/4 lg:hidden flex flex-col items-end justify-start  p-4 gap-6  bg-white'>
+          <ul className='absolute w-1/4 lg:hidden flex flex-col items-end justify-start  p-4 gap-6 '>
             {navLinks.map((item) => (
               <li key={item.label}>
                 <Link
                   href={item.href}
-                  className='font-montserrat leading-normal text-lg text-slate-gray'
+                  className='font-montserrat leading-normal text-lg    '
                 >
                   {item.label}
                 </Link>
@@ -83,10 +108,12 @@ const Navbar = () => {
             ))}
             <Link
               href='/login'
-              className='font-montserrat leading-normal text-lg text-slate-gray'
+              className='font-montserrat leading-normal text-lg   '
             >
               Login
             </Link>
+
+            <ModeToggle />
           </ul>
         </div>
       )}
