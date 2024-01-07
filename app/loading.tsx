1d9de9b0cee5084f5b28c7ad5196e136a2aa4d2a
Ajarek <ajarek@poczta.onet.pl>
2024-01-07 22:01:55 +0100

    added modifications loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       
-      <Skeleton className="w-[300px] h-[300px] rounded-full" />
+      <Skeleton className="w-[300px] h-[300px] rounded-full flex items-center justify-center text-xl text-slate-400 ">  Processing...</Skeleton>
     </div>
   )
 }
